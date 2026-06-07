f3d8aa97ec483234a1d69509544ddeca5fe3aca7
Prasanna-cpu <kumar22maran@gmail.com>
2026-06-07 16:10:35 +0530

    commit frontend_setup_and_more

---
+++ frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatPage : React.FunctionComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChatPage;
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage: React.FunctionComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LoginPage;
